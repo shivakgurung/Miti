570059c528894cd979c15a6335b6e1b8d3428911
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-10-18 11:44:56 +0545

    feat: :sparkles: Added announcement

--- frontend/src/Body.tsx
+++ frontend/src/Body.tsx
@@ -1,32 +1,37 @@
-import DateConverter from "./pages/DateConverter.tsx";
-import About from "./pages/About.tsx";
+import { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import AnnouncementAlert from "./components/AnnouncementAlert.tsx";
 import { DarkModeContext } from "./components/DarkModeProvider.tsx";
+import Footer from "./components/Footer.tsx";
+import Navbar from "./components/Navbar.tsx";
+import About from "./pages/About.tsx";
+import DateConverter from "./pages/DateConverter.tsx";
+import GoogleApiDisclosure from "./pages/GoogleApiDisclosure.tsx";
 import Home from "./pages/Home.tsx";
 import PrivacyPolicy from "./pages/PrivacyPolicy.tsx";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.tsx";
-import { useContext } from "react";
-import Footer from "./components/Footer.tsx";
-import GoogleApiDisclosure from "./pages/GoogleApiDisclosure.tsx";
 
 const Body = () => {
-  const { darkMode } = useContext(DarkModeContext);
+	const { darkMode } = useContext(DarkModeContext);
 
-  return (
-    <div className={(darkMode ? "dark" : "") + " flex min-h-screen flex-col"}>
-      <Navbar />
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/:pageType?/:BSYear?/:BSMonth?" element={<Home />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/converter" element={<DateConverter />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/google-api-disclosure" element={<GoogleApiDisclosure />} />
-        </Routes>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className={(darkMode ? "dark" : "") + " flex min-h-screen flex-col"}>
+			<AnnouncementAlert />
+			<Navbar />
+			<div className="flex-grow">
+				<Routes>
+					<Route path="/:pageType?/:BSYear?/:BSMonth?" element={<Home />} />
+					<Route path="/privacy" element={<PrivacyPolicy />} />
+					<Route path="/converter" element={<DateConverter />} />
+					<Route path="/about" element={<About />} />
+					<Route
+						path="/google-api-disclosure"
+						element={<GoogleApiDisclosure />}
+					/>
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Body;
